d299eb8ba9e8ca76a7612aa838cacb1a36ee2c65
Lauren Noles <Lauren_noles1@baylor.edu>
2023-08-09 20:38:53 -0600

    commit

--- Arrays1.py
+++ Arrays1.py
@@ -3,35 +3,41 @@ import numpy as np
 
 ## Step 1: Create a 4x3 array of all 2s
 print("-----------------------------------------------   STEP ONE   -----------------------------------------------")
-
+step1array = np.full((4, 3), 2)
+print(step1array)
 print()
 
 ## Step 2: Create a 3x4 array with a range from 0 to 110 where each number increases by 10
 print("-----------------------------------------------   STEP TWO   -----------------------------------------------")
-
+step2array = np.arange(0, 111, 10).reshape(3,4)
+print(step2array)
 print() 
 
 ## Step 3: Change the layout of the above array to be 4x3, store it in a new array
 print("-----------------------------------------------   STEP THREE   -----------------------------------------------")
-
+step3array = step2array.reshape(4,3)
+print(step3array)
 print()
 
 ## Step 4: Multiply every elemnt of the above array by 3 and store the new values in a different array
 print("-----------------------------------------------   STEP FOUR   -----------------------------------------------")
-
+step4array = step3array * 3
+print(step4array)
 print()
 
 ## Step 5: Multiply your array from step one by your array from step 2
 print("-----------------------------------------------   STEP FIVE   -----------------------------------------------")
-
+#step5array = step1array * step2array
+#ERROR
 ## This errored out... why?
-print()
+print('The arrays do not have matching dimensions.')
 
 ## Step 6: Comment out your code from Step 5 and then multiply your array from step 1 by your array from step 3
 print("-----------------------------------------------   STEP SIX   -----------------------------------------------")
-
+step6array = step1array * step3array
+print(step6array)
 ## this worked! why?
-print()
+print('The arrays have matching dimensions.')
 
 
 
--- Arrays2.py
+++ Arrays2.py
@@ -6,49 +6,61 @@ salesArray = np.array([[150.68,207.99,107.88,58.99,60.59],[20.89,98.99,258.62,19
 
 ## Step 1: Print the total sales for the store.
 print("-----------------------------------------------   STEP ONE   -----------------------------------------------")
-
+step1array = np.sum(salesArray)
+print(step1array)
 print()
 
 ## Step 2: What was Superstore's smallest and largest sale? Print them.
 print("-----------------------------------------------   STEP TWO   -----------------------------------------------")
-
+smallest = np.min(salesArray)
+largest = np.max(salesArray)
+print('Smallest sale: ', smallest)
+print('Largest sale: ', largest)
 print()
 
 ## Step 3: Print an array that will show which sales are greater than or equal to $100.
 print("-----------------------------------------------   STEP THREE  -----------------------------------------------")
-
+step3array = salesArray >= 100
 print()
 
 ## Step 4: Print the total sales for each register.
 print("-----------------------------------------------   STEP FOUR   -----------------------------------------------")
-
+step4array = np.sum(salesArray, axis = 1)
+print(step4array)
 print()
 
 ## Step 5: Superstore is a cashless store and needs to calculate their owed credit card fees. Each sale is subject to a 2% credit card fee.
 #           Using the salesArray, create a new array that stores the 2% fee for each sale and register. Print the array and then print the total fees.
 print("-----------------------------------------------   STEP FIVE  -----------------------------------------------")
-
+step5array = salesArray * .02
+print(step5array)
+step5fees = np.sum(step5array)
+print(step5fees)
 print()
 
 ## Step 6: Using your fee array and salesArray, calculate how much profit Superstore made for each sale after paying credit card fees. Store this in a new array and print it.
 print("-----------------------------------------------   STEP SIX  -----------------------------------------------")
-
+step6array = salesArray - step5fees
+print(step6array)
 print()
 
 ## Step 7: Print the sales only for the second and forth cash register
 print("-----------------------------------------------   STEP SEVEN  -----------------------------------------------")
-
+step7array = salesArray[[1,3], :]
+print(step7array)
 print()
 
 ## Step 8: Superstore has added a 5th cash register who's data is stored in the array newRegister. Add the new register to the original array. Print the updated salesArray.
 print("-----------------------------------------------   STEP EIGHT  -----------------------------------------------")
 newRegister = np.array([17.89,13.59,107.89,176.88,56.78])
-
+salesArray = np.vstack((salesArray, newRegister))
+print(salesArray)
 print()
 
 ## Step 9: Register #3 had an error and recorded it's fourth sale ($200.14) incorrectly. The sale should have been $20.14. Update the array to correct this error.
 #           Print the array before and after the update to see the change.
 print("-----------------------------------------------   STEP NINE  -----------------------------------------------")
-
+salesArray[2,3] = 20.14
+print(salesArray)
 print()
 
